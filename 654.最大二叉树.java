37158794e86734e368de42c1aa519e6268d27bb7
gdpolar <2249303992@qq.com>
2023-03-20 18:57:19 +0800

    update

---
+++ 654.最大二叉树.java
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=java
+ *
+ * [654] 最大二叉树
+ *
+ * https://leetcode.cn/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (82.82%)
+ * Likes:    648
+ * Dislikes: 0
+ * Total Accepted:    184.2K
+ * Total Submissions: 222.7K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+ * 
+ * 
+ * 创建一个根节点，其值为 nums 中的最大值。
+ * 递归地在最大值 左边 的 子数组前缀上 构建左子树。
+ * 递归地在最大值 右边 的 子数组后缀上 构建右子树。
+ * 
+ * 
+ * 返回 nums 构建的 最大二叉树 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [3,2,1,6,0,5]
+ * 输出：[6,3,5,null,2,0,null,null,1]
+ * 解释：递归调用如下所示：
+ * - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+ * ⁠   - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+ * ⁠       - 空数组，无子节点。
+ * ⁠       - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+ * ⁠           - 空数组，无子节点。
+ * ⁠           - 只有一个元素，所以子节点是一个值为 1 的节点。
+ * ⁠   - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+ * ⁠       - 只有一个元素，所以子节点是一个值为 0 的节点。
+ * ⁠       - 空数组，无子节点。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,1]
+ * 输出：[3,null,2,null,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 1000
+ * nums 中的所有整数 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int[] nums;
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        this.nums = nums;
+        return constructWithBeginAndEnd(0, nums.length);
+    }
+
+    public TreeNode constructWithBeginAndEnd(int begin, int end) {
+        if (begin >= end) {
+            return null;
+        }
+        int rootIndex = getMaxIndex(nums, begin, end);
+        TreeNode root = new TreeNode(nums[rootIndex]);
+        // 此处左闭右开
+        root.left = constructWithBeginAndEnd(begin, rootIndex);
+        root.right = constructWithBeginAndEnd(rootIndex + 1, end);
+        return root;
+    }
+
+    public int getMaxIndex(int[] nums, int begin, int end) {
+        int maxIndex = begin;
+        for (int i = begin; i < end; i++) {
+            if (nums[maxIndex] < nums[i]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+}
+// @lc code=end
+
